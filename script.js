cc74ebc3a05412ff94df76688d898f14de2dd400
Cassandra <cparisi1290@gmail.com>
2021-09-07 17:52:36 -0500

    grab elements to add js to, iterate over each small cup element, add click event to element

---
+++ script.js
@@ -0,0 +1,14 @@
+const smallCups = document.querySelectorAll('.cup-small');
+const liters = document.getElementById('liters');
+const percentage = document.getElementById('percentage');
+const remained = document.getElementById('remained');
+
+smallCups.forEach((cup, idx) => {
+  //   console.log(idx);
+
+  cup.addEventListener('click', () => highlightCups(idx));
+});
+
+function highlightCups(idx) {
+  console.log(idx);
+}
